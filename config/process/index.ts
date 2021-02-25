f6c16a1fbf2eddc54df3a853dad4711848cfe2e1
Arttu Pennanen <arttu@pennanen.org>
2021-02-25 15:50:59 +0200

    ssl hotfix

--- config/process/index.ts
+++ config/process/index.ts
@@ -8,22 +8,19 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const sslFilePaths = {
-    privateKey: '/etc/letsencrypt/live/typechase.pennanen.dev/privkey.pem',
-    certificate: '/etc/letsencrypt/live/typechase.pennanen.dev/cert.pem',
-    ca: '/etc/letsencrypt/live/typechase.pennanen.dev/chain.pem'
+    privateKey: '/letsencrypt/privkey.pem',
+    certificate: '/letsencrypt/cert.pem',
+    ca: '/letsencrypt/chain.pem'
 }
 
 let ssl = {
-    privateKey: fs.existsSync(sslFilePaths.privateKey)
-        ? fs.readFileSync('/etc/letsencrypt/live/typechase.pennanen.dev/privkey.pem', 'utf8')
-        : null,
-    certificate: fs.existsSync(sslFilePaths.certificate)
-        ? fs.readFileSync('/etc/letsencrypt/live/typechase.pennanen.dev/cert.pem', 'utf8')
-        : null,
-    ca: fs.existsSync(sslFilePaths.ca)
-        ? fs.readFileSync('/etc/letsencrypt/live/typechase.pennanen.dev/chain.pem', 'utf8')
-        : null
+    privateKey: fs.existsSync(sslFilePaths.privateKey) ? fs.readFileSync(sslFilePaths.privateKey, 'utf8') : null,
+    certificate: fs.existsSync(sslFilePaths.certificate) ? fs.readFileSync(sslFilePaths.certificate, 'utf8') : null,
+    ca: fs.existsSync(sslFilePaths.ca) ? fs.readFileSync(sslFilePaths.ca, 'utf8') : null
 }
+Object.keys(ssl).forEach((val) => {
+    console.log(val, ssl[val])
+})
 
 let session = {
     secret: process.env.SESSION_SECRET
